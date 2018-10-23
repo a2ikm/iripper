353d3d48095229de5fedf7cb772de6c12c47d8b0
Masato Ikeda <masato.ikeda@gmail.com>
2018-10-23 22:59:31 +0900

    Initial commit

---
+++ lib/ripper/repl.rb
@@ -0,0 +1,7 @@
+require "ripper/repl/version"
+
+module Ripper
+  module Repl
+    # Your code goes here...
+  end
+end
---
+++ lib/ripper/repl/version.rb
@@ -0,0 +1,5 @@
+module Ripper
+  module Repl
+    VERSION = "0.1.0"
+  end
+end
